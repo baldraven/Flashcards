55a21f7192fb8f5ead2ba8c9019632f47aafb941
Killua <valentin.thiebaux.1@telecomnancy.eu>
2023-01-03 15:23:49 +0100

    Bouton Affichage réponse working

--- src/main/java/eu/telecomnancy/flashcards/controller/ControllerLearning.java
+++ src/main/java/eu/telecomnancy/flashcards/controller/ControllerLearning.java
@@ -1,11 +1,18 @@
-package eu.telecomnancy.flashcards;
+package eu.telecomnancy.flashcards.controller;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class ControllerLearning
 {
     public void ControllerLearning()
     {
-        
+
+    }
+    public void AfficherRep(Label Rep, GridPane gridP)
+    {
+        Rep.setVisible(true);
+        gridP.setVisible(true);
     }
 }
--- src/main/java/eu/telecomnancy/flashcards/view/ViewLearning.java
+++ src/main/java/eu/telecomnancy/flashcards/view/ViewLearning.java
@@ -3,7 +3,8 @@ package eu.telecomnancy.flashcards.view;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import eu.telecomnancy.flashcards.Observer;
 import eu.telecomnancy.flashcards.controller.ControllerLearning;
 
@@ -11,7 +12,10 @@ public class ViewLearning implements Observer
 {
     @FXML
     public Button repButton;
-
+    @FXML
+    public Label Rep;
+    @FXML
+    public GridPane gridP;
     private ControllerLearning Controler;
 
     public void ViewLearning()
@@ -22,7 +26,7 @@ public class ViewLearning implements Observer
     @FXML
     public void AffRep()
     {
-        Controler.AfficherRep();
+        Controler.AfficherRep(Rep, gridP);
         this.reagir();
     }
     public void reagir()
