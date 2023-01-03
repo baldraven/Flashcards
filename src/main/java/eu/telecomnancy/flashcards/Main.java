0f236820353b0945de61c026b447fb8e10bf9dba
Killua <valentin.thiebaux.1@telecomnancy.eu>
2023-01-03 10:20:19 +0100

    Ajout des premiers FXML

--- src/main/java/eu/telecomnancy/flashcards/Main.java
+++ src/main/java/eu/telecomnancy/flashcards/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ApprentissageMode.fxml"));
 
         Parent root = loader.load();
         Scene scene = new Scene(root);
