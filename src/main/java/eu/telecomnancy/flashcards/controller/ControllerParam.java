64c5f97574b67d669ac9dafd653ae7e118b4a1c9
Thiebaux Valentin <Valentin.thiebaux.1@telecomnancy.eu>
2023-01-05 17:12:50 +0100

    Param timer working

--- src/main/java/eu/telecomnancy/flashcards/controller/ControllerParam.java
+++ src/main/java/eu/telecomnancy/flashcards/controller/ControllerParam.java
@@ -80,21 +80,34 @@ public class ControllerParam extends AbstractControllerMenu implements Observer,
         try
         {
             int i = Integer.parseInt(sec.getText());
-            param.setIsSecond(isSec.isSelected());
-            param.setOneTime(oneSelec.isSelected());
-            param.setSecond(i);
-            param.updateParam();
-            Alert alert = new Alert(AlertType.CONFIRMATION);
-            alert.setTitle("Enregistrer !");
-            alert.setContentText("Les paramètres ont été modifié avec succès !");
-            alert.showAndWait();
-            this.model.getViewChanger().setView("DeckList");
+            if(i < 3 || i > 60)
+            {
+                Alert alert = new Alert(AlertType.ERROR);
+                alert.setTitle("Erreur");
+                alert.setContentText("Plage de valeur incorrect.");
+                alert.setHeaderText(null);
+                alert.showAndWait();
+            }
+            else
+            {
+                param.setIsSecond(isSec.isSelected());
+                param.setOneTime(oneSelec.isSelected());
+                param.setSecond(i);
+                param.updateParam();
+                Alert alert = new Alert(AlertType.INFORMATION);
+                alert.setTitle("Enregistrer !");
+                alert.setContentText("Les paramètres ont été modifié avec succès !");
+                alert.setHeaderText(null);
+                alert.showAndWait();
+                this.model.getViewChanger().setView("DeckList");
+            }
         }
         catch(NumberFormatException nfe)
         {
             Alert alert = new Alert(AlertType.ERROR);
             alert.setTitle("Erreur");
             alert.setContentText("Le nombre de seconde indiqué est incorrecte.");
+            alert.setHeaderText(null);
             alert.showAndWait();
         }
     }
